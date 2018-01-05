7a430d72efb5b041b5448f8a9d3a831822b8a586
MartinMyr <martin.myrmarker@gmail.com>
2018-01-05 17:04:03 +0100

    menu problem fixed
    Added menu loop to function

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $( document ).ready(function() {
         
  
         //Startup functions
-
+        meny();
 
             var slideIndex = 0;
         carousel();
@@ -54,64 +54,65 @@ $( document ).ready(function() {
         if (slideIndex > x.length) {slideIndex = 1} 
         x[slideIndex-1].style.display = "block"; 
         setTimeout(carousel, 6000);
-        }
+        };
   
   
   
    
         //Funktion för sortering av Huvud och under meny
-        underKat.sort(function(a, b){
-            var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
-            if (nameA < nameB) 
-                return -1 
-            if (nameA > nameB)
-                return 1
-            return 0 
-        });
+        function meny(){
+            underKat.sort(function(a, b){
+                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+                if (nameA < nameB) 
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0 
+            });
 
-        huvudKat.sort(function(a, b){
-            var menyValA=a.menyVal.toLowerCase(), menyValB=b.menyVal.toLowerCase()
-            if (menyValA < menyValB) 
-                return -1 
-            if (menyValA > menyValB)
-                return 1
-            return 0 
-        });
-         //Funktion för sortering av Huvud och under meny SLUT
+            huvudKat.sort(function(a, b){
+                var menyValA=a.menyVal.toLowerCase(), menyValB=b.menyVal.toLowerCase()
+                if (menyValA < menyValB) 
+                    return -1 
+                if (menyValA > menyValB)
+                    return 1
+                return 0 
+            });
+            //Funktion för sortering av Huvud och under meny SLUT
 
 
-        for (i = 0; i < huvudKat.length; i++) { 
-         
-
-            var headMenu = '<div class="dropdown"><button class="dropbtn">'+ huvudKat[i].menyVal;
-            headMenu += '<div class="dropdown-content' + i + '">';
-            headMenu += '</button></div></div> ';
-            $(".navbar").append(headMenu);
-           
-           
+            for (i = 0; i < huvudKat.length; i++) { 
             
+
+                var headMenu = '<div class="dropdown"><button class="dropbtn">'+ huvudKat[i].menyVal;
+                headMenu += '<div class="dropdown-content' + i + '">';
+                headMenu += '</button></div></div> ';
+                $(".navbar").append(headMenu);
+            
+            
+                
+            };
+                
+            for (i = 0; i < underKat.length; i++){
+                var underMenu;
+                underMeny = '<a href="#">';
+                underMeny += underKat[i].name + "</a>";      
+            
+                if(underKat[i].huvudkategori === 1){
+                $(".dropdown-content0").append(underMeny );
+                }else if(underKat[i].huvudkategori === 2){
+                    $(".dropdown-content1").append(underMeny );
+                }else if(underKat[i].huvudkategori === 3){
+                    $(".dropdown-content2").append(underMeny );
+                }else if (underKat[i].huvudkategori === 4){
+                    $(".dropdown-content3").append(underMeny );
+                };  
+                
+            
+            
+            };
+
         };
-            
-        for (i = 0; i < underKat.length; i++){
-            var underMenu;
-            underMeny = '<a href="#">';
-            underMeny += underKat[i].name + "</a>";      
-        
-            if(underKat[i].huvudkategori === 1){
-            $(".dropdown-content0").append(underMeny );
-            }else if(underKat[i].huvudkategori === 2){
-                $(".dropdown-content1").append(underMeny );
-            }else if(underKat[i].huvudkategori === 3){
-                $(".dropdown-content2").append(underMeny );
-            }else if (underKat[i].huvudkategori === 4){
-                $(".dropdown-content3").append(underMeny );
-            };  
-            
-           
-           
-        };
-
-      
         
         $(this).on("click", function(){
             for (i = 0; i < produkter.length; i++){
